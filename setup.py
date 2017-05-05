15f792c4ef6b234f387df640abddfba0cb916e9c
Tim <tim@preply.com>
2017-05-05 19:03:44 +0300

    Adds PyPI files.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='dj_anonymizer',
+    packages=['dj_anonymizer'],
+    version='0.1',
+    description='A random test lib',
+    author='Tim Pagurets',
+    author_email='tim@preply.com',
+    url='https://github.com/knowledge-point/dj_anonymizer',
+    download_url='https://github.com/knowledge-point/dj_anonymizer/archive/0.1.tar.gz',
+    keywords=['django', 'database', 'anonymization'],
+    classifiers=[],
+)
