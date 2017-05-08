32d4ae57dd7b5b1f97b51f36fc910e09ae07dce4
Tim <tim@preply.com>
2017-05-08 11:12:11 +0300

    small fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Tim Pagurets',
     author_email='tim@preply.com',
     url='https://github.com/knowledge-point/dj_anonymizer',
-    download_url='https://github.com/knowledge-point/dj_anonymizer/archive/0.1.tar.gz',
+    download_url='https://github.com/knowledge-point/dj_anonymizer/archive/0.1.1.tar.gz',
     keywords=['django', 'database', 'anonymization'],
     classifiers=[],
     install_requires=['django-bulk-update==1.1.10']
