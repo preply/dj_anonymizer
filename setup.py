0241e4ef44265aea9432fb65ff6d289d7bdbedba
Tim <tim@preply.com>
2017-05-22 15:34:32 +0300

    Added extra descriptions into setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,24 @@ setup(
     name='dj_anonymizer',
     packages=['dj_anonymizer'],
     version='0.1.1',
-    description='A random test lib',
+    description='This project helps anonymize production database with fake data of any kind.',
+    license='MIT',
     author='Tim Pagurets',
     author_email='tim@preply.com',
     url='https://github.com/knowledge-point/dj_anonymizer',
     download_url='https://github.com/knowledge-point/dj_anonymizer/archive/0.1.1.tar.gz',
-    keywords=['django', 'database', 'anonymization'],
-    classifiers=[],
+    keywords=['django', 'data', 'database', 'anonymization', 'dj-anonymizer'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Topic :: Database'
+    ],
     install_requires=['django-bulk-update==1.1.10']
 )
