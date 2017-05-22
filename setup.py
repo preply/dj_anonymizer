b90faea635235643b1c425f15789493190160886
Tim <tim@preply.com>
2017-05-22 15:41:16 +0300

    new release

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
     name='dj_anonymizer',
     packages=['dj_anonymizer'],
-    version='0.1.1',
+    version='0.1.2',
     description='This project helps anonymize production database with fake data of any kind.',
     license='MIT',
     author='Tim Pagurets',
     author_email='tim@preply.com',
     url='https://github.com/knowledge-point/dj_anonymizer',
-    download_url='https://github.com/knowledge-point/dj_anonymizer/archive/0.1.1.tar.gz',
+    download_url='https://github.com/knowledge-point/dj_anonymizer/archive/0.1.2.tar.gz',
     keywords=['django', 'data', 'database', 'anonymization', 'dj-anonymizer'],
     classifiers=[
         'Development Status :: 4 - Beta',
