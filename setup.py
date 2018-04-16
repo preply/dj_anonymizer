1ec350bfcc5dcdfbc64fe0be1f1276954b5fe62a
pngnviko <viktoria.r@preply.com>
2018-04-16 15:21:25 +0300

    draft new releas

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
 setup(
     name='dj_anonymizer',
     packages=['dj_anonymizer'],
-    version='0.1.3',
+    version='0.1.4',
     description='This project helps anonymize production database with fake data of any kind.',
     long_description=(read('README.md')),
     license='MIT',
