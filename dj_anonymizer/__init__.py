1a9884f1a4bb63f77fcbbc9823b5ca7f47baea9f
pngnviko <viktoria.r@preply.com>
2018-04-24 11:57:09 +0300

    edd import

--- dj_anonymizer/__init__.py
+++ dj_anonymizer/__init__.py
@@ -2,3 +2,4 @@ from dj_anonymizer.register_models import (
     register_anonym, register_clean, register_clean_with_rules, register_skip, AnonymBase
 )
 from dj_anonymizer import anonym_field
+from dj_anonymizer.django_command import CommandAnonymize
