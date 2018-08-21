ffb5a8c828bdd26b6a06b9b26dec1401c269f174
chinskiy <chinskiy93@gmail.com>
2018-08-21 15:26:19 +0300

    isort

--- example/polls/migrations/0001_initial.py
+++ example/polls/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-from django.db import migrations, models
 import django.db.models.deletion
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
