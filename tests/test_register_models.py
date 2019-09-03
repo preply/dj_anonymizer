43d76aa7df6a746ab7702e55c834805f9c94dc10
chinskiy <chinskiy93@gmail.com>
2019-09-03 22:23:11 +0300

    add tests for register_clean

--- tests/test_register_models.py
+++ tests/test_register_models.py
@@ -1,7 +1,34 @@
 import mock
 from django.contrib.auth.models import Group, Permission, User
+from django.db.models.query import QuerySet
 
 from dj_anonymizer import register_models
+
+
+def test_register_clean(mocker):
+    clean_mock = mock.MagicMock()
+    mocker.patch(
+        'dj_anonymizer.anonymizer.Anonymizer.clean_models',
+        clean_mock
+    )
+    register_models.register_clean([
+        (User, register_models.AnonymBase),
+        (Permission, register_models.AnonymBase(truncate=True)),
+        (Group, register_models.AnonymBase())
+    ])
+
+    assert clean_mock._mock_mock_calls[0][1][0] == \
+        'django.contrib.auth.models.User'
+    assert isinstance(clean_mock._mock_mock_calls[0][1][1], QuerySet)
+    assert clean_mock._mock_mock_calls[0][1][1].truncate is False
+    assert clean_mock._mock_mock_calls[1][1][0] == \
+        'django.contrib.auth.models.Permission'
+    assert isinstance(clean_mock._mock_mock_calls[1][1][1], QuerySet)
+    assert clean_mock._mock_mock_calls[1][1][1].truncate is True
+    assert clean_mock._mock_mock_calls[2][1][1].truncate is False
+    assert clean_mock._mock_mock_calls[2][1][0] == \
+        'django.contrib.auth.models.Group'
+    assert isinstance(clean_mock._mock_mock_calls[2][1][1], QuerySet)
 
 
 def test_register_skip(mocker):
