23013bab230278c1134190cac24a13bcbb72d8cb
chinskiy <chinskiy@preply.com>
2022-01-14 18:39:31 +0200

    add basic test for register_anonym

--- tests/test_register_models.py
+++ tests/test_register_models.py
@@ -1,13 +1,87 @@
 import pytest
+import datetime
+import types
 from django.contrib.auth.models import Group, Permission, User
 from django.db.models.query import QuerySet
 
-from dj_anonymizer import register_models
+from dj_anonymizer import register_models, fields
 from dj_anonymizer.anonymizer import Anonymizer
 
 
 @pytest.mark.django_db
-def test_register_clean(mocker):
+def test_register_anonym():
+    class UserAnonym(register_models.AnonymBase):
+        email = fields.string('test_email_{seq}@preply.com',
+                              seq_callback=datetime.datetime.now)
+        username = fields.string('test_username_{seq}@preply.com',
+                                 seq_callback=datetime.datetime.now)
+        first_name = fields.string('first name {seq}')
+        last_name = fields.string('last name {seq}')
+        password = fields.password('password')
+        is_staff = fields.function(lambda: False)
+
+        class Meta:
+            exclude_fields = ['is_active', 'is_superuser',
+                              'last_login', 'date_joined']
+    register_models.register_anonym([
+        (User, UserAnonym)
+    ])
+
+    assert len(Anonymizer.clean_models) == 0
+    assert len(Anonymizer.skip_models) == 0
+    assert len(Anonymizer.anonym_models) == 1
+
+    assert 'django.contrib.auth.models.User' in \
+        Anonymizer.anonym_models.keys()
+
+    assert isinstance(
+        Anonymizer.anonym_models[
+            'django.contrib.auth.models.User'
+        ].Meta.queryset,
+        QuerySet
+    )
+
+    assert Anonymizer.anonym_models[
+        'django.contrib.auth.models.User'
+    ].Meta.exclude_fields == [
+        'is_active', 'is_superuser', 'last_login', 'date_joined'
+    ]
+
+    assert isinstance(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].email,
+        types.GeneratorType
+    )
+    assert isinstance(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].username,
+        types.GeneratorType
+    )
+    assert isinstance(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].first_name,
+        types.GeneratorType
+    )
+    assert isinstance(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].last_name,
+        types.GeneratorType
+    )
+    assert isinstance(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].password,
+        types.GeneratorType
+    )
+    assert isinstance(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].is_staff,
+        types.GeneratorType
+    )
+
+    assert next(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].first_name
+    ) == 'first name 0'
+    assert next(
+        Anonymizer.anonym_models['django.contrib.auth.models.User'].is_staff
+    ) is False
+
+
+@pytest.mark.django_db
+def test_register_clean():
     register_models.register_clean([
         (User, register_models.AnonymBase),
         (Permission, register_models.AnonymBase(truncate=True)),
@@ -50,7 +124,7 @@ def test_register_clean(mocker):
 
 
 @pytest.mark.django_db
-def test_register_clean_duplicate(mocker):
+def test_register_clean_duplicate():
     with pytest.raises(ValueError):
         register_models.register_clean([
             (User, register_models.AnonymBase),
@@ -61,7 +135,7 @@ def test_register_clean_duplicate(mocker):
 
 
 @pytest.mark.django_db
-def test_register_skip(mocker):
+def test_register_skip():
     register_models.register_skip([User, Permission, Group])
 
     assert len(Anonymizer.clean_models) == 0
@@ -74,6 +148,6 @@ def test_register_skip(mocker):
 
 
 @pytest.mark.django_db
-def test_register_skip_duplicate(mocker):
+def test_register_skip_duplicate():
     with pytest.raises(ValueError):
         register_models.register_skip([User, Permission, Group, Group])
