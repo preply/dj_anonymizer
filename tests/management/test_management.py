072dd686a670d50800a82247f317333002958aab
chinskiy <chinskiy@preply.com>
2022-01-27 02:00:42 +0200

    cover with tests

--- tests/management/test_management.py
+++ tests/management/test_management.py
@@ -17,10 +17,20 @@ class TestAnonymizeDB():
                   anonymize_called, clean_called):
         anonymize_mock = mock.Mock()
         clean_mock = mock.Mock()
+        anonym_models = dict({'k': 'v'})
+        clean_models = dict({'k': 'v'})
         mocker.patch(
-            'dj_anonymizer.anonymizer.Anonymizer.anonymize', anonymize_mock)
+            'dj_anonymizer.anonymizer.Anonymizer.anonymize', anonymize_mock
+        )
         mocker.patch(
-            'dj_anonymizer.anonymizer.Anonymizer.clean', clean_mock)
+            'dj_anonymizer.anonymizer.Anonymizer.clean', clean_mock
+        )
+        mocker.patch(
+            'dj_anonymizer.anonymizer.Anonymizer.anonym_models', anonym_models
+        )
+        mocker.patch(
+            'dj_anonymizer.anonymizer.Anonymizer.clean_models', clean_models
+        )
 
         management.call_command(
             'anonymize_db',
@@ -31,3 +41,29 @@ class TestAnonymizeDB():
 
         assert anonymize_mock.called is anonymize_called
         assert clean_mock.called is clean_called
+
+    def test_base_empty_lists(self, mocker):
+        anonymize_mock = mock.Mock()
+        clean_mock = mock.Mock()
+        anonym_models = dict()
+        clean_models = dict()
+        mocker.patch(
+            'dj_anonymizer.anonymizer.Anonymizer.anonymize', anonymize_mock
+        )
+        mocker.patch(
+            'dj_anonymizer.anonymizer.Anonymizer.clean', clean_mock
+        )
+        mocker.patch(
+            'dj_anonymizer.anonymizer.Anonymizer.anonym_models', anonym_models
+        )
+        mocker.patch(
+            'dj_anonymizer.anonymizer.Anonymizer.clean_models', clean_models
+        )
+
+        management.call_command(
+            'anonymize_db',
+            soft_mode=True
+        )
+
+        assert anonymize_mock.called is False
+        assert clean_mock.called is False
