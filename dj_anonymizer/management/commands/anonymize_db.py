367ae69ba8ce20d4de62aa12f75f0cfb57120667
chinskiy <chinskiy@preply.com>
2022-01-31 18:06:05 +0200

    add --only parameter

--- dj_anonymizer/management/commands/anonymize_db.py
+++ dj_anonymizer/management/commands/anonymize_db.py
@@ -12,32 +12,45 @@ class Command(BaseCommand):
         parser.add_argument('-a', '--action',
                             help='"anonymize" models or "clean" models '
                                  '(default: both actions will be applied)')
-        parser.add_argument('-s', '--soft_mode', action='store_true',
-                            help='Exception will not be raised if not all '
-                                 'project models are registered')
         parser.add_argument('-c', '--check_only', action='store_true',
                             help='Check that all models are registered '
                                  'correctly')
+        parser.add_argument('-o', '--only',
+                            help='Execute specified for dj_anonymizer action '
+                                 'only on specified model')
+        parser.add_argument('-s', '--soft_mode', action='store_true',
+                            help='Exception will not be raised if not all '
+                                 'project models are registered')
 
     def handle(self, *args, **options):
         self.action = options['action']
-        self.soft_mode = options['soft_mode']
         self.check_only = options['check_only']
+        self.only = options['only']
+        self.soft_mode = options['soft_mode']
         start = time.time()
-        anonymizer = Anonymizer(soft_mode=self.soft_mode)
+        anonymizer = Anonymizer(
+            soft_mode=self.soft_mode,
+            only=self.only
+        )
         self.stdout.write('Check pass successfully')
         self.stdout.write(f'{"=-" * 25}=')
         if self.check_only is True:
             return
         if self.action is None or self.action == 'anonymize':
-            if len(anonymizer.anonym_models) > 0:
+            if (
+                (self.only is None)
+                or (self.only is not None and self.only in Anonymizer.anonym_model)
+            ) and len(anonymizer.anonym_models) > 0:
                 self.stdout.write('Anonymizing started')
-                anonymizer.anonymize()
+                anonymizer.anonymize(only=self.only)
                 self.stdout.write('Anonymizing finished')
         if self.action is None or self.action == 'clean':
-            if len(anonymizer.clean_models) > 0:
+            if (
+                (self.only is None)
+                or (self.only is not None and self.only in Anonymizer.clean_models)
+            ) and len(anonymizer.anonym_models) > 0:
                 self.stdout.write('Cleaning started')
-                anonymizer.clean()
+                anonymizer.clean(only=self.only)
                 self.stdout.write('Cleaning finished')
         end = time.time() - start
         self.stdout.write(f'{"=-" * 25}=')
