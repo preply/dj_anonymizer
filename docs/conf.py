93d63c2f9b41df80ce8f5cc8d83f7f21969611a5
Marcos Estévez <marcos.estevez@preply.com>
2024-01-09 11:12:11 +0100

    Support Truncate with Cascade (#73)
    * Support Truncate with Cascade
    
    ---------
    
    Co-authored-by: chinskiy <4370169+chinskiy@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Preply'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
