fdaad6bc234b5108e4c05f4e6d140e49406b6203
Dmytro Vdovychynskyi <4370169+chinskiy@users.noreply.github.com>
2024-01-09 14:13:26 +0200

    0.6.0 (#75)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='dj_anonymizer',
     packages=['dj_anonymizer'],
     include_package_data=True,
-    version='0.6.0-dev',
+    version='0.6.0',
     description='This project helps anonymize production database '
                 + 'with fake data of any kind.',
     long_description=(read('README.md')),
