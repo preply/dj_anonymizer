909b9faea0c5f07d9d344a0e087ea2515ece2ccd
chinskiy <4370169+chinskiy@users.noreply.github.com>
2024-04-18 16:48:26 +0300

    Fix deprecation warnings

--- example/anonymizer/base.py
+++ example/anonymizer/base.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import
 
-import datetime
-
 from django.contrib.admin.models import LogEntry
 from django.contrib.auth.models import Group, Permission, User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sessions.models import Session
+from django.utils import timezone
 
 from dj_anonymizer import fields
 from dj_anonymizer.register_models import (
@@ -18,9 +17,9 @@ from dj_anonymizer.register_models import (
 
 class UserAnonym(AnonymBase):
     email = fields.string('test_email_{seq}@preply.com',
-                          seq_callback=datetime.datetime.now)
+                          seq_callback=timezone.now)
     username = fields.string('test_username_{seq}@preply.com',
-                             seq_callback=datetime.datetime.now)
+                             seq_callback=timezone.now)
     first_name = fields.string('first name {seq}')
     last_name = fields.string('last name {seq}')
     password = fields.password('password')
